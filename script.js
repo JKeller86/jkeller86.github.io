aa76688af91e01f7c2bb6df5afb9fe78cd3dabec
Jon Keller <jkeller86@me.com>
2017-08-29 19:39:45 -0400

    Add files via upload
    Added Firebase backend functionality: 
    1) User Authentication 
    2) User Logout
    3) User Data - Log activities 
    4) View logs 
    5) Minor tweaks

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function testing(){
-	alert("testing");
-}
 
-function submitted(){
-	alert("Your workout has been logged! Great job!");
+
+// function submitted(){
+// 	alert("Your workout has been logged! Great job!");
+// }
+
+firebase.auth().onAuthStateChanged(function(user){
+	window.signedInUser=user
+})
+
+function logout(){
+	firebase.auth().signOut()
+	.then(function(){
+		window.signedInUser=null
+		console.info("Signed out.")
+		window.location="index.html"
+	}).catch(console.error)
 }
