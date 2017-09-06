a6d83479a053960cc436fc8987aee023877ed756
Jon Keller <jkeller86@me.com>
2017-09-05 21:26:31 -0400

    Add files via upload
    Cleaned up formatting

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-
-
 // function submitted(){
 // 	alert("Your workout has been logged! Great job!");
 // }
-
-firebase.auth().onAuthStateChanged(function(user){
-	window.signedInUser=user
+firebase.auth().onAuthStateChanged(function(user) {
+    window.signedInUser = user
 })
 
-function logout(){
-	firebase.auth().signOut()
-	.then(function(){
-		window.signedInUser=null
-		console.info("Signed out.")
-		window.location="index.html"
-	}).catch(console.error)
+function logout() {
+    firebase.auth().signOut().then(function() {
+        window.signedInUser = null
+        console.info("Signed out.")
+        window.location = "index.html"
+    }).catch(console.error)
 }
 
-function goToApp(){
-	window.location="app.html"
+function goToApp() {
+    window.location = "app.html"
 }
 
-function goToLogs(){
-	window.location="logs.html"
+function goToLogs() {
+    window.location = "logs.html"
 }
 
-function goToSubmission(){
-	window.location="submission.html"
+function goToSubmission() {
+    window.location = "submission.html"
 }
 
-function getStarted(){
-	window.location="signup.html"
+function getStarted() {
+    window.location = "signup.html"
 }
