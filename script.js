c397ea09d0d692bfec2ba0cdbceac46d150318bc
Jon Keller <jkeller86@me.com>
2017-10-08 17:05:56 -0400

    Code Clean Up
    Cleaned up stray / poorly formatted code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // function submitted(){
-// 	alert("Your workout has been logged! Great job!");
+//  alert("Your workout has been logged! Great job!");
 // }
 firebase.auth().onAuthStateChanged(function(user) {
     window.signedInUser = user
@@ -29,36 +29,31 @@ function getStarted() {
     window.location = "signup.html"
 }
 
-
 function hasClass(lengthCell, className) {
-  if (lengthCell.classList)
-    return lengthCell.classList.contains(className)
-  else
-    return !!lengthCell.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+    if (lengthCell.classList) return lengthCell.classList.contains(className)
+    else return !!lengthCell.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
 }
 
 function addClass(lengthCell, className) {
-  if (lengthCell.classList)
-    lengthCell.classList.add(className)
-  else if (!hasClass(lengthCell, className)) lengthCell.className += " " + className
+    if (lengthCell.classList) lengthCell.classList.add(className)
+    else if (!hasClass(lengthCell, className)) lengthCell.className += " " + className
 }
 
 function removeClass(lengthCell, className) {
-  if (lengthCell.classList)
-    lengthCell.classList.remove(className)
-  else if (hasClass(lengthCell, className)) {
-    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
-    lengthCell.className=lengthCell.className.replace(reg, ' ')
-  }
+    if (lengthCell.classList) lengthCell.classList.remove(className)
+    else if (hasClass(lengthCell, className)) {
+        var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+        lengthCell.className = lengthCell.className.replace(reg, ' ')
+    }
 }
 
-function total(){
-  var cls = document.getElementById("myLogTable").getElementsByTagName("td");
-  var sum = 0;
-  for (var i = 0; i < cls.length; i++){
-  if(cls[i].className == "countable"){
-    sum += isNaN(cls[i].innerHTML) ? 0 : parseInt(cls[i].innerHTML);
+function total() {
+    var cls = document.getElementById("myLogTable").getElementsByTagName("td");
+    var sum = 0;
+    for (var i = 0; i < cls.length; i++) {
+        if (cls[i].className == "countable") {
+            sum += isNaN(cls[i].innerHTML) ? 0 : parseInt(cls[i].innerHTML);
+        }
     }
-  }
-  alert('You have worked out for a total of ' + sum +  ' minutes!'); 
+    alert('You have exercised for a total of ' + sum + ' minutes!');
 }
