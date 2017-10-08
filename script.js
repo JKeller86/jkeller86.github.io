a1925f5dc8f9481ea31b49c84071040849dd88b8
Jon Keller <jkeller86@me.com>
2017-10-08 16:46:44 -0400

    Add files via upload
    Added script to sum values in table based off of classes added to dynamic HTML

--- script.js
+++ script.js
@@ -28,3 +28,37 @@ function goToSubmission() {
 function getStarted() {
     window.location = "signup.html"
 }
+
+
+function hasClass(lengthCell, className) {
+  if (lengthCell.classList)
+    return lengthCell.classList.contains(className)
+  else
+    return !!lengthCell.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+}
+
+function addClass(lengthCell, className) {
+  if (lengthCell.classList)
+    lengthCell.classList.add(className)
+  else if (!hasClass(lengthCell, className)) lengthCell.className += " " + className
+}
+
+function removeClass(lengthCell, className) {
+  if (lengthCell.classList)
+    lengthCell.classList.remove(className)
+  else if (hasClass(lengthCell, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    lengthCell.className=lengthCell.className.replace(reg, ' ')
+  }
+}
+
+function total(){
+  var cls = document.getElementById("myLogTable").getElementsByTagName("td");
+  var sum = 0;
+  for (var i = 0; i < cls.length; i++){
+  if(cls[i].className == "countable"){
+    sum += isNaN(cls[i].innerHTML) ? 0 : parseInt(cls[i].innerHTML);
+    }
+  }
+  alert('You have worked out for a total of ' + sum +  ' minutes!'); 
+}
